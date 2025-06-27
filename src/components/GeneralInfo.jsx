90a89e9418b0d58e96f725304db808df77d37014
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-06-27 13:43:35 -0400

    Refactor to toggle between edit and form

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,4 +1,8 @@
-function GeneralInfo({ fullName, email, phone, setInfo, setShowOutput }) {
+import { useState } from "react";
+
+function GeneralInfo({ fullName, email, phone, setInfo }) {
+  const [isEditing, setIsEditing] = useState(true);
+
   function setGeneralInfo(event) {
     const { name, value } = event.target;
 
@@ -8,52 +12,58 @@ function GeneralInfo({ fullName, email, phone, setInfo, setShowOutput }) {
     }));
   }
 
-  function handleSubmit() {
-    setShowOutput((previousState) => ({
-      ...previousState,
-      generalInfo: true,
-    }));
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsEditing(false);
   }
 
   return (
     <div>
-      <fieldset>
-        <legend>General information</legend>
-        <div>
-          <label>Full name:</label>{" "}
-          <input
-            type="text"
-            name="fullName"
-            value={fullName}
-            onChange={setGeneralInfo}
-            required
-          />
-        </div>
-        <div>
-          <label>Email:</label>{" "}
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={setGeneralInfo}
-            required
-          />
-        </div>
-        <div>
-          <label>Phone:</label>{" "}
-          <input
-            type="tel"
-            name="phone"
-            value={phone}
-            onChange={setGeneralInfo}
-            maxLength="10"
-            required
-          />
-        </div>
-        <div>
-          <input type="submit" value="Update" onClick={handleSubmit}></input>
-        </div>
-      </fieldset>
+      {isEditing ? (
+        <>
+          <div>
+            <label>Full name:</label>{" "}
+            <input
+              type="text"
+              name="fullName"
+              value={fullName}
+              onChange={setGeneralInfo}
+              required
+            />
+          </div>
+          <div>
+            <label>Email:</label>{" "}
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={setGeneralInfo}
+              required
+            />
+          </div>
+          <div>
+            <label>Phone:</label>{" "}
+            <input
+              type="tel"
+              name="phone"
+              value={phone}
+              onChange={setGeneralInfo}
+              maxLength="10"
+              required
+            />
+          </div>
+          <div>
+            <input type="submit" value="Submit" onClick={handleSubmit} />
+          </div>
+        </>
+      ) : (
+        <>
+          <h1>{fullName}</h1>
+          <p>{email}</p>
+          <p>{phone}</p>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+        </>
+      )}
     </div>
   );
 }
