0c2b14a24167a1949d235aa60640cfc679d3c354
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-06-27 13:13:48 -0400

    Create button logic and output for GeneralInfo

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,37 @@ function App() {
     phone: "",
   });
 
+  const [showOutput, setShowOutput] = useState({
+    generalInfo: false,
+    education: false,
+    experience: false,
+  });
+
   return (
     <div className="app">
       <h1>CV Builder</h1>
-      <GeneralInfo
-        fullName={generalInfo.fullName}
-        email={generalInfo.email}
-        phone={generalInfo.phone}
-        setInfo={setGeneralInfo}
-        info={generalInfo}
-      />
-      <Education />
-      <Experience />
+      <div className="wrapper">
+        <div className="input">
+          <GeneralInfo
+            fullName={generalInfo.fullName}
+            email={generalInfo.email}
+            phone={generalInfo.phone}
+            setInfo={setGeneralInfo}
+            setShowOutput={setShowOutput}
+          />
+          <Education />
+          <Experience />
+        </div>
+        <div className="output">
+          {showOutput.generalInfo && (
+            <div>
+              <h1>{generalInfo.fullName}</h1>
+              <p>{generalInfo.email}</p>
+              <p>{generalInfo.phone}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,4 +1,4 @@
-function GeneralInfo({ fullName, email, phone, setInfo }) {
+function GeneralInfo({ fullName, email, phone, setInfo, setShowOutput }) {
   function setGeneralInfo(event) {
     const { name, value } = event.target;
 
@@ -7,45 +7,53 @@ function GeneralInfo({ fullName, email, phone, setInfo }) {
       [name]: value,
     }));
   }
-  function renderForm(generalInfo) {
-    console.log({ fullName, email, phone });
+
+  function handleSubmit() {
+    setShowOutput((previousState) => ({
+      ...previousState,
+      generalInfo: true,
+    }));
   }
+
   return (
     <div>
-      <div>
-        <label>Full name:</label>{" "}
-        <input
-          type="text"
-          name="fullName"
-          value={fullName}
-          onChange={setGeneralInfo}
-          required
-        />
-      </div>
-      <div>
-        <label>Email:</label>{" "}
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={setGeneralInfo}
-          required
-        />
-      </div>
-      <div>
-        <label>Phone:</label>{" "}
-        <input
-          type="tel"
-          name="phone"
-          value={phone}
-          onChange={setGeneralInfo}
-          maxLength="10"
-          required
-        />
-      </div>
-      <div>
-        <input type="submit" value="Update" onClick={renderForm}></input>
-      </div>
+      <fieldset>
+        <legend>General information</legend>
+        <div>
+          <label>Full name:</label>{" "}
+          <input
+            type="text"
+            name="fullName"
+            value={fullName}
+            onChange={setGeneralInfo}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>{" "}
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={setGeneralInfo}
+            required
+          />
+        </div>
+        <div>
+          <label>Phone:</label>{" "}
+          <input
+            type="tel"
+            name="phone"
+            value={phone}
+            onChange={setGeneralInfo}
+            maxLength="10"
+            required
+          />
+        </div>
+        <div>
+          <input type="submit" value="Update" onClick={handleSubmit}></input>
+        </div>
+      </fieldset>
     </div>
   );
 }
